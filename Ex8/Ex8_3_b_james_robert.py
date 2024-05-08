8e6e3aed82644197dfee06b720e31decd5bc62c2
JAANKl <89450417+JAANKl@users.noreply.github.com>
2024-05-08 11:58:15 +0200

    included mc

--- Ex8/Ex8_3_b_james_robert.py
+++ Ex8/Ex8_3_b_james_robert.py
@@ -29,6 +29,11 @@ def u_exact(x):
 
 # takes in all values of u = (u_j^n)_j at time n and returns vector of fluxes (F_{j+1/2})_j
 
+def general_minmod(a):
+    # if not all entries have the same sign, return 0
+    minmod_condition = np.logical_or(np.all(a>0, axis=1), np.all(a<0, axis=1))
+    return np.min(np.abs(a), axis = 1)*(minmod_condition)
+
 def rusanov_flux(u_left, u_right, dx, dt):
     return (f(u_left) + f(u_right)) / 2 - np.max([np.abs(f_prime(u_left)), np.abs(f_prime(u_right))]) / 2 * (
             u_right - u_left)
@@ -72,6 +77,19 @@ def sigma_van_leer(u, dx):
     sigma_inside = (r+np.abs(r))/(1+np.abs(r))
     return np.concatenate([[sigma_inside[0]], sigma_inside, [sigma_inside[-1]]])
 
+def sigma_mc(u, dx):
+    du = np.diff(u)
+    right_diff = du[1:]
+    left_diff = du[:-1]
+    central_diff = (right_diff+left_diff)/2
+    # boundary
+    data = np.concatenate((right_diff[:, None], central_diff[:, None], left_diff[:, None]),axis=1)/dx
+    sigma_inside = general_minmod(data)
+    return np.concatenate([[sigma_inside[0]], sigma_inside, [sigma_inside[-1]]])
+
+def mc(a,b):
+    return np.maximum(0, np.sign(a) * np.minimum(np.abs(2*a), np.minimum(np.abs((a+b)/2), np.abs(2*b))))
+
 
 def sigma(u, dx):
     du = np.diff(u)
@@ -104,8 +122,8 @@ numerical_solutions = []
 
 # number of decimals for reporting values
 precision = 4
-sigma_func_list = [sigma_minmod, sigma_van_leer, sigma_superbee]
-sigma_names = ["minmod", "van leer", "superbee"]
+sigma_func_list = [sigma_minmod, sigma_van_leer, sigma_superbee, sigma_mc]
+sigma_names = ["minmod", "van leer", "superbee", "MC"]
 
 for sigma_func in sigma_func_list:
     for i, N in enumerate(mesh_sizes):
